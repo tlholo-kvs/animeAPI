0c43ceb13feb4080cbe6d395f56c8e444c15b050
tlholo-kvs <tlholokgoele@hotmail.com>
2023-05-05 12:37:40 +0200

    feat(program-cs): updated  program.cs to contain code for building web application

--- animeAPI/Program.cs
+++ animeAPI/Program.cs
@@ -1,5 +1,20 @@
-public class Program
+using animeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container 
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SimpleAnimeListContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("simple-anime-list")));
+
+var app = builder.Build();
+
+public partial class Program
 {
+
+     
     public static void Main(string[] args) { 
         
     }
