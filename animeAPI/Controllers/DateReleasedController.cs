92ac853c4d9058095c9ce2181201fbfe72407963
tlholo-kvs <tlholokgoele@hotmail.com>
2023-05-10 15:46:59 +0200

    feat(CRUD-logic): Added CRUD operations for DateReleasedController

--- animeAPI/Controllers/DateReleasedController.cs
+++ animeAPI/Controllers/DateReleasedController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using animeAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace animeAPI.Controllers
 {
@@ -7,5 +9,52 @@ namespace animeAPI.Controllers
     [ApiController]
     public class DateReleasedController : ControllerBase
     {
+        private readonly SimpleAnimeListContext _context;
+
+        public DateReleasedController(SimpleAnimeListContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<DateReleased>> Get() 
+        {
+            return await _context.DateReleaseds.ToListAsync(); ;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(DateReleased dateReleased)
+        {
+            _context.Add(dateReleased);
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(DateReleased dateReleasedData)
+        { 
+            var dateReleased = await _context.DateReleaseds.FindAsync(dateReleasedData);
+            if(dateReleased == null)
+                return BadRequest();
+
+            dateReleased.FirstAiring = dateReleasedData.FirstAiring;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id) 
+        {
+            var dateReleased = await _context.DateReleaseds.FindAsync(id);
+            if (dateReleased == null) return NotFound();
+
+            _context.DateReleaseds.Remove(dateReleased);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
