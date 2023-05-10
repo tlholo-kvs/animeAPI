80553368535c755372e4a3f5b3a90c390b007542
tlholo-kvs <tlholokgoele@hotmail.com>
2023-05-10 15:23:14 +0200

    feat(api-routes): Added CRUD functionality to AnimeTitlesController

--- animeAPI/Controllers/AnimeTitleController.cs
+++ animeAPI/Controllers/AnimeTitleController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using animeAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace animeAPI.Controllers
 {
@@ -7,5 +9,73 @@ namespace animeAPI.Controllers
     [ApiController]
     public class AnimeTitleController : ControllerBase
     {
+        private readonly SimpleAnimeListContext _context;
+
+        public AnimeTitleController(SimpleAnimeListContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AnimeTitle>> GetAnimeTitles()
+        {
+            return await _context.AnimeTitles.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AnimeTitle>> GetAnimeTitle(int id)
+        {
+            var animeTitle = await _context.AnimeTitles.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (animeTitle == null) {
+                return NotFound();
+            }
+
+            return Ok(animeTitle);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(AnimeTitle animeTitle)
+        {
+            _context.Add(animeTitle);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAnimeTitle(AnimeTitle animeTitleData)
+        {
+            if (animeTitleData == null || animeTitleData.Id == 0) 
+            {
+                return BadRequest();    
+            }
+
+            var animeTitle = await _context.AnimeTitles.FindAsync(animeTitleData.Id);
+            if (animeTitle == null) 
+            {
+                return NotFound();
+            }
+
+            animeTitle.Name = animeTitleData.Name;
+            animeTitle.Id = animeTitleData.Id;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var animeTitle = await _context.AnimeTitles.FindAsync(id);
+
+            if(animeTitle == null)  return NotFound();
+
+            _context.AnimeTitles.Remove(animeTitle);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
